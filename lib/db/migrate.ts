9e84b862cd192865220fc4d7d17cc02096e5170d
Sowparna Shome <soupornoshome@gmail.com>
2025-05-05 23:55:00 +0530

    scripts and custom migration add

---
+++ lib/db/migrate.ts
@@ -0,0 +1,23 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in .env.local");
+}
+
+async function runMigration() {
+    console.log("🔃 Starting migration...");
+    try {
+        const sql = neon(process.env.DATABASE_URL!);
+        const db = drizzle(sql);
+        console.log("📂 Running migrations from ./drizzle folder");
+        await migrate(db, { migrationsFolder: "./drizzle" });
+        console.log("✅ Database migration completed successfully!");
+    } catch (error) {
+        console.error("❌ Migration failed:", error);
+        process.exit(1);
+    }
+}
+
+runMigration();
