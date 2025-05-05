197f314d21fec97a959d9adb15fe32bb541cfd34
Sowparna Shome <soupornoshome@gmail.com>
2025-05-06 00:06:05 +0530

    dotenv import and path fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
     throw new Error("😖 Database url is not set in .env");
 }
 export default defineConfig({
     out: './drizzle',
-    schema: './src/db/schema.ts',
+    schema: './lib/db/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
     },
     migrations: {
-        table: "__drizzle_migration",
+        table: "__drizzle_migrations",
         schema: "public"
     },
     verbose: true,
--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,9 +1,12 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set in .env.local");
+  throw new Error("DATABASE_URL is not set in .env");
 }
 
 async function runMigration() {
