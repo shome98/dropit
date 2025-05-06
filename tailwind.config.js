02772393e91d935413e4d915a8bfaa82f4850525
Sowparna Shome <soupornoshome@gmail.com>
2025-05-06 22:31:09 +0530

    npx heroui-cli@latest add input

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const {heroui} = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/(card|divider|ripple).js"
+    "./node_modules/@heroui/theme/dist/components/(card|divider|input|ripple|form).js"
 ],
   theme: {
     extend: {},
